ade13fcbbb98905e02d02671e0fe1827d3aa0d77
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-10-06 10:12:01 -0500

    se cambia de asset a secure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <title>Portafolio</title>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
