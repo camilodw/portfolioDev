1b0c1917ec34bc2b48ef4ba09ffc4beec85aac7b
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-10-30 02:31:30 -0500

    se agrego vista de 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+
+    <title>Error 404</title>
+  </head>
+  <body>
+    <div class="container mt-5 pt-5 text-center">
+        <div class="alert alert-danger text-center">
+            <h2 class="display-3">404</h2>
+            <p class="display-5">Oops! No encontramos lo que estas buscando.</p>
+        </div>
+        <a href="/" class="btn btn-outline-primary">Volver al inicio</a>
+    </div>
+
+  </body>
+</html>
